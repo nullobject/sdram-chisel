0f424b7086295a16e7e4a4fe1162652542ece4f9
Joshua Bassett <hello@joshbassett.info>
2020-10-11 17:43:17 +1100

    Add CacheMem module

---
+++ src/main/scala/mem/CacheMem.scala
@@ -0,0 +1,237 @@
+/*
+ *    __   __     __  __     __         __
+ *   /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *   \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *    \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *     \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *    ______     ______       __     ______     ______     ______
+ *   /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *   \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *    \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *     \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ *  https://joshbassett.info
+ *  https://twitter.com/nullobject
+ *  https://github.com/nullobject
+ *
+ *  Copyright (c) 2020 Josh Bassett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package mem
+
+import chisel3._
+import chisel3.util._
+
+/**
+ * Represents the cache configuration.
+ *
+ * @param inAddrWidth The width of the input address bus.
+ * @param inDataWidth The width of the input data bus.
+ * @param outAddrWidth The width of the output address bus.
+ * @param outDataWidth The width of the output data bus.
+ * @param depth The number of entries in the cache.
+ * @param offset The offset of the output address.
+ */
+case class CacheConfig(inAddrWidth: Int,
+                       inDataWidth: Int,
+                       outAddrWidth: Int,
+                       outDataWidth: Int,
+                       depth: Int,
+                       offset: Int = 0) {
+  /** The width of the words in a cache line */
+  val wordWidth = inDataWidth
+  /** The number of words in a cache line */
+  val numWords = outDataWidth / inDataWidth
+  /** The width of a cache address index */
+  val indexWidth = log2Ceil(depth)
+  /** The width of a cache address offset */
+  val offsetWidth = log2Ceil(numWords)
+  /** The width of a cache tag */
+  val tagWidth = outAddrWidth - indexWidth - offsetWidth
+  /** The width of a cache line */
+  val lineWidth = wordWidth * numWords
+}
+
+/** Represents the data stored in each cache entry. */
+class CacheLine(private val config: CacheConfig) extends Bundle {
+  /** The words in the cache line */
+  val words = Vec(config.numWords, Bits(config.wordWidth.W))
+}
+
+/** Represents the location of a word stored within the cache. */
+class CacheAddress(private val config: CacheConfig) extends Bundle {
+  /** The most significant bits of the address */
+  val tag = UInt(config.tagWidth.W)
+  /** The index of the cache entry within the cache */
+  val index = UInt(config.indexWidth.W)
+  /** The offset of the word within the cache line */
+  val offset = UInt(config.offsetWidth.W)
+}
+
+/** Represents an entry stored in the cache. */
+class CacheEntry(private val config: CacheConfig) extends Bundle {
+  /** Flag to indicate whether the cache entry is valid */
+  val valid = Bool()
+  /** Flag to indicate whether the cache entry is dirty */
+  val dirty = Bool()
+  /** The most significant bits of the address */
+  val tag = UInt(config.tagWidth.W)
+  /** The cache line */
+  val line = new CacheLine(config)
+}
+
+/**
+ * A direct-mapped cache memory.
+ *
+ * A cache can be used to speed up access to a high latency memory device (e.g. SDRAM), by keeping a
+ * copy of frequently accessed data. It supports both read and write operations.
+ *
+ * Cache entries are stored in BRAM, which means that reading/writing to a memory address that is
+ * already stored in the cache (cache hit) only takes one clock cycle.
+ *
+ * Reading/writing to memory address that isn't stored in the cache (cache miss) is slower, as the
+ * data must first be fetched from the high latency memory device (line fill). Although, subsequent
+ * reads/writes to the same address will be much faster.
+ *
+ * @param config The cache configuration.
+ */
+class CacheMem(config: CacheConfig) extends Module {
+  val io = IO(new Bundle {
+    /** Input port */
+    val in = Flipped(AsyncReadWriteMemIO(config.inAddrWidth, config.inDataWidth))
+    /** Output port */
+    val out = AsyncReadWriteMemIO(config.outAddrWidth, config.outDataWidth)
+    /** Debug port */
+    val debug = Output(new Bundle {
+      val idle = Bool()
+      val check = Bool()
+      val lineFill = Bool()
+      val evict = Bool()
+    })
+  })
+
+  // Input/output port aliases
+  val in = io.in
+  val out = io.out
+  val inAddr = io.in.addr.asTypeOf(new CacheAddress(config))
+
+  // States
+  val initState :: idleState :: checkState :: lineFillState :: evictState :: Nil = Enum(5)
+
+  // Wires
+  val entry = Wire(new CacheEntry(config))
+
+  // Registers
+  val stateReg = RegInit(initState)
+  val writeReg = RegEnable(in.wr, stateReg === idleState)
+  val addrReg = RegEnable(inAddr, stateReg === idleState)
+  val dataReg = RegEnable(in.din, stateReg === idleState)
+
+  // Counters
+  val (counterValue, counterWrap) = Counter(stateReg === initState, config.depth)
+
+  // Cache entry memory
+  val mem = SyncReadMem(config.depth, new CacheEntry(config), SyncReadMem.WriteFirst)
+
+  // Control signals
+  val dirty = entry.dirty && entry.tag =/= addrReg.tag
+  val hit = entry.valid && entry.tag === addrReg.tag
+  val miss = !hit
+  val waitReq = (dirty || miss) && out.waitReq
+
+  // Set output address to cache entry address during an eviction, otherwise use the cache address
+  val outAddr = Mux(stateReg === checkState && dirty, entry.tag, addrReg.tag) ## addrReg.index
+
+  // Read cache entry during the idle state, so it's valid in time for the check state
+  entry := mem.read(inAddr.index, stateReg === idleState)
+
+  // Initialize cache entries
+  when(stateReg === initState) {
+    mem.write(counterValue, 0.U.asTypeOf(new CacheEntry(config)))
+  }
+
+  // Fill cache line from memory
+  when(stateReg === lineFillState && out.valid) {
+    entry.valid := true.B
+    entry.dirty := false.B
+    entry.tag := addrReg.tag
+    entry.line := out.dout.asTypeOf(new CacheLine(config))
+    mem.write(addrReg.index, entry)
+  }
+
+  // Update word in the cache line
+  when(stateReg === checkState && hit && writeReg) {
+    entry.dirty := true.B
+    entry.line.words(addrReg.offset) := dataReg
+    mem.write(addrReg.index, entry)
+  }
+
+  // FSM
+  switch(stateReg) {
+    // Initialize cache
+    is(initState) {
+      when(counterWrap) { stateReg := idleState }
+    }
+
+    // Wait for a request
+    is(idleState) {
+      when(in.rd || in.wr) { stateReg := checkState }
+    }
+
+    // Check cache entry
+    is(checkState) {
+      when(waitReq) {
+        // Wait for request
+      }.elsewhen(dirty) {
+        stateReg := evictState
+      }.elsewhen(miss) {
+        stateReg := lineFillState
+      }.otherwise {
+        stateReg := idleState
+      }
+    }
+
+    // Fill cache line
+    is(lineFillState) {
+      when(out.valid) { stateReg := checkState }
+    }
+
+    // Evict dirty cache entry
+    is(evictState) {
+      when(!out.waitReq) { stateReg := lineFillState }
+    }
+  }
+
+  // Outputs
+  in.valid := stateReg === checkState && hit && !writeReg
+  in.waitReq := stateReg =/= idleState
+  in.dout := entry.line.words(addrReg.offset)
+  out.rd := (stateReg === checkState && !dirty && miss) || stateReg === evictState
+  out.wr := stateReg === checkState && dirty
+  out.addr := outAddr + config.offset.U
+  out.din := entry.line.asUInt
+  io.debug.idle := stateReg === idleState
+  io.debug.check := stateReg === checkState
+  io.debug.lineFill := stateReg === lineFillState
+  io.debug.evict := stateReg === evictState
+
+  printf(p"CacheMem(state: $stateReg, write: $writeReg, addr: $addrReg, data: $dataReg, entry: $entry, hit: $hit, dirty: $dirty)\n")
+}
---
+++ src/test/scala/mem/CacheMemTest.scala
@@ -0,0 +1,408 @@
+/*
+ *   __   __     __  __     __         __
+ *  /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *  \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *   \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *    \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *   ______     ______       __     ______     ______     ______
+ *  /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *  \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *   \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *    \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ * https://joshbassett.info
+ * https://twitter.com/nullobject
+ * https://github.com/nullobject
+ *
+ * Copyright (c) 2020 Josh Bassett
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package mem
+
+import chisel3._
+import chiseltest._
+import org.scalatest._
+
+trait CacheMemHelpers {
+  protected val cacheConfig = CacheConfig(
+    inAddrWidth = 8,
+    inDataWidth = 4,
+    outAddrWidth = 16,
+    outDataWidth = 8,
+    depth = 8,
+    offset = 0x1000
+  )
+
+  protected def mkCacheMem(config: CacheConfig = cacheConfig) = new CacheMem(config)
+
+  protected def readCache(dut: CacheMem, addr: Int) = {
+    dut.io.in.rd.poke(true.B)
+    dut.io.in.addr.poke(addr.U)
+    dut.clock.step()
+    dut.io.in.rd.poke(false.B)
+    val result = dut.io.in.dout.peek().litValue()
+    dut.clock.step()
+    result
+  }
+
+  protected def writeCache(dut: CacheMem, addr: Int, data: Int) = {
+    dut.io.in.wr.poke(true.B)
+    dut.io.in.addr.poke(addr.U)
+    dut.io.in.din.poke(data.U)
+    dut.clock.step()
+    dut.io.in.wr.poke(false.B)
+    dut.clock.step()
+  }
+
+  protected def writeCacheLine(dut: CacheMem, addr: Int, data: Int) = {
+    dut.io.in.rd.poke(true.B)
+    dut.io.in.addr.poke(addr.U)
+    dut.io.out.waitReq.poke(false.B)
+    dut.clock.step()
+    dut.io.in.rd.poke(false.B)
+    dut.clock.step()
+    dut.io.out.valid.poke(true.B)
+    dut.io.out.dout.poke(data.U)
+    dut.clock.step()
+    dut.io.out.valid.poke(false.B)
+    dut.clock.step()
+  }
+
+  protected def waitForIdle(dut: CacheMem) =
+    while(!dut.io.debug.idle.peek().litToBoolean) { dut.clock.step() }
+
+  protected def waitForCheck(dut: CacheMem) =
+    while(!dut.io.debug.check.peek().litToBoolean) { dut.clock.step() }
+
+  protected def waitForLineFill(dut: CacheMem) =
+    while(!dut.io.debug.lineFill.peek().litToBoolean) { dut.clock.step() }
+
+  protected def waitForEvict(dut: CacheMem) =
+    while(!dut.io.debug.evict.peek().litToBoolean) { dut.clock.step() }
+}
+
+class CacheMemTest extends FlatSpec with ChiselScalatestTester with Matchers with CacheMemHelpers {
+  behavior of "FSM"
+
+  it should "deassert the wait signal during the idle state" in {
+    test(mkCacheMem()) { dut =>
+      dut.io.in.rd.poke(true.B)
+      dut.io.in.waitReq.expect(true.B)
+      waitForIdle(dut)
+      dut.io.in.waitReq.expect(false.B)
+      waitForCheck(dut)
+      dut.io.in.waitReq.expect(true.B)
+    }
+  }
+
+  it should "move to the check state after a read request" in {
+    test(mkCacheMem()) { dut =>
+      dut.io.in.rd.poke(true.B)
+      waitForIdle(dut)
+      dut.clock.step()
+      dut.io.debug.check.expect(true.B)
+    }
+  }
+
+  it should "move to the check state after a write request" in {
+    test(mkCacheMem()) { dut =>
+      dut.io.in.wr.poke(true.B)
+      waitForIdle(dut)
+      dut.clock.step()
+      dut.io.debug.check.expect(true.B)
+    }
+  }
+
+  it should "move to the line fill state after a read miss" in {
+    test(mkCacheMem()) { dut =>
+      dut.io.in.rd.poke(true.B)
+      waitForCheck(dut)
+      dut.clock.step()
+      dut.io.debug.lineFill.expect(true.B)
+    }
+  }
+
+  it should "move to the line fill state after a write miss" in {
+    test(mkCacheMem()) { dut =>
+      dut.io.in.wr.poke(true.B)
+      waitForCheck(dut)
+      dut.clock.step()
+      dut.io.debug.lineFill.expect(true.B)
+    }
+  }
+
+  it should "move to the evict state after a dirty write hit" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+      writeCache(dut, 0x01, 3)
+      dut.io.in.wr.poke(true.B)
+      dut.io.in.addr.poke(16.U)
+      waitForCheck(dut)
+      dut.clock.step()
+      dut.io.debug.evict.expect(true.B)
+    }
+  }
+
+  it should "move to the line fill state after an eviction" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+      writeCache(dut, 0x01, 3)
+      dut.io.in.wr.poke(true.B)
+      dut.io.in.addr.poke(16.U)
+      waitForEvict(dut)
+      dut.clock.step()
+      dut.io.debug.lineFill.expect(true.B)
+    }
+  }
+
+  it should "return to the check state after a line fill" in {
+    test(mkCacheMem()) { dut =>
+      dut.io.in.rd.poke(true.B)
+      waitForLineFill(dut)
+      dut.io.out.valid.poke(true.B)
+      dut.clock.step()
+      dut.io.debug.check.expect(true.B)
+    }
+  }
+
+  it should "return to the idle state after a read hit" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+      dut.io.in.rd.poke(true.B)
+      waitForCheck(dut)
+      dut.clock.step()
+      dut.io.debug.idle.expect(true.B)
+    }
+  }
+
+  it should "return to the idle state after a write hit" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+      dut.io.in.wr.poke(true.B)
+      waitForCheck(dut)
+      dut.clock.step()
+      dut.io.debug.idle.expect(true.B)
+    }
+  }
+
+  behavior of "read hit"
+
+  it should "read from the cache" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+
+      // Read
+      dut.io.in.rd.poke(true.B)
+      dut.io.in.addr.poke(1.U)
+      dut.clock.step()
+      dut.io.in.rd.poke(false.B)
+      dut.io.in.addr.poke(0.U)
+
+      // No line fill
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(false.B)
+
+      // Valid
+      dut.io.in.valid.expect(true.B)
+      dut.io.in.dout.expect(2.U)
+    }
+  }
+
+  it should "evict a dirty cache entry before reading from the cache" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+      writeCache(dut, 0x01, 3)
+
+      // Read
+      dut.io.in.rd.poke(true.B)
+      dut.io.in.addr.poke(16.U)
+      dut.clock.step()
+      dut.io.in.rd.poke(false.B)
+      dut.io.in.addr.poke(0.U)
+
+      // Evict
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(true.B)
+      dut.io.out.addr.expect(0x1000.U)
+      dut.io.out.din.expect(0x31.U)
+      dut.clock.step()
+
+      // Line fill
+      dut.io.out.rd.expect(true.B)
+      dut.io.out.wr.expect(false.B)
+      dut.io.out.addr.expect(0x1008.U)
+      dut.clock.step()
+      dut.io.out.valid.poke(true.B)
+      dut.io.out.dout.poke(0x43.U)
+      dut.io.in.valid.expect(false.B)
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(false.B)
+      dut.clock.step()
+
+      // Valid
+      dut.io.out.valid.poke(false.B)
+      dut.io.in.valid.expect(true.B)
+      dut.io.in.dout.expect(3.U)
+    }
+  }
+
+  behavior of "read miss"
+
+  it should "fill a cache line before reading from the cache" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+
+      // Read
+      dut.io.in.rd.poke(true.B)
+      dut.io.in.addr.poke(0.U)
+      dut.clock.step()
+      dut.io.in.rd.poke(false.B)
+
+      // Line fill
+      dut.io.out.rd.expect(true.B)
+      dut.io.out.wr.expect(false.B)
+      dut.io.out.addr.expect(0x1000.U)
+      dut.clock.step()
+      dut.io.out.valid.poke(true.B)
+      dut.io.out.dout.poke(0x21.U)
+      dut.io.in.valid.expect(false.B)
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(false.B)
+      dut.clock.step()
+
+      // Valid
+      dut.io.out.valid.poke(false.B)
+      dut.io.in.valid.expect(true.B)
+      dut.io.in.dout.expect(1.U)
+    }
+  }
+
+  behavior of "write hit"
+
+  it should "write to the cache" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+
+      // Write
+      dut.io.in.wr.poke(true.B)
+      dut.io.in.addr.poke(1.U)
+      dut.io.in.din.poke(3.U)
+      dut.clock.step()
+      dut.io.in.wr.poke(false.B)
+      dut.io.in.addr.poke(0.U)
+      dut.io.in.din.poke(0.U)
+
+      // No line fill
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(false.B)
+      dut.clock.step()
+
+      readCache(dut, 0x00) shouldBe 1
+      readCache(dut, 0x01) shouldBe 3
+    }
+  }
+
+  it should "evict a dirty cache entry before writing to the cache" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+      writeCacheLine(dut, 0x00, 0x21)
+      writeCache(dut, 0x01, 3)
+
+      // Write
+      dut.io.in.wr.poke(true.B)
+      dut.io.in.addr.poke(16.U)
+      dut.io.in.din.poke(4.U)
+      dut.clock.step()
+      dut.io.in.wr.poke(false.B)
+      dut.io.in.addr.poke(0.U)
+      dut.io.in.din.poke(0.U)
+
+      // Evict
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(true.B)
+      dut.io.out.addr.expect(0x1000.U)
+      dut.io.out.din.expect(0x31.U)
+      dut.clock.step()
+
+      // Line fill
+      dut.io.out.rd.expect(true.B)
+      dut.io.out.wr.expect(false.B)
+      dut.io.out.addr.expect(0x1008.U)
+      dut.clock.step()
+      dut.io.out.valid.poke(true.B)
+      dut.io.out.dout.poke(0x65.U)
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(false.B)
+      dut.clock.step()
+
+      // Not valid
+      dut.io.out.valid.poke(false.B)
+      dut.io.in.valid.expect(false.B)
+      dut.clock.step()
+
+      readCache(dut, 0x10) shouldBe 4
+      readCache(dut, 0x11) shouldBe 6
+    }
+  }
+
+  behavior of "write miss"
+
+  it should "fill a cache line before writing to the cache" in {
+    test(mkCacheMem()) { dut =>
+      waitForIdle(dut)
+
+      // Write
+      dut.io.in.wr.poke(true.B)
+      dut.io.in.addr.poke(1.U)
+      dut.io.in.din.poke(3.U)
+      dut.clock.step()
+      dut.io.in.wr.poke(false.B)
+      dut.io.in.addr.poke(0.U)
+      dut.io.in.din.poke(0.U)
+
+      // Line fill
+      dut.io.out.rd.expect(true.B)
+      dut.io.out.wr.expect(false.B)
+      dut.io.out.addr.expect(0x1000.U)
+      dut.clock.step()
+      dut.io.out.valid.poke(true.B)
+      dut.io.out.dout.poke(0x21.U)
+      dut.io.out.rd.expect(false.B)
+      dut.io.out.wr.expect(false.B)
+      dut.clock.step()
+
+      // Not valid
+      dut.io.out.valid.poke(false.B)
+      dut.io.in.valid.expect(false.B)
+      dut.clock.step()
+
+      readCache(dut, 0) shouldBe 1
+      readCache(dut, 1) shouldBe 3
+    }
+  }
+}
